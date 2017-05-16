34713638f8bd09820d1fbb96089bf5fe329ef52e
Wojciech Dudek <wdudek82@gmail.com>
2017-05-16 10:13:54 +0200

    Added exception handling for incorrect inputs

--- learning-modules/copy_biggest.py
+++ learning-modules/copy_biggest.py
@@ -4,32 +4,44 @@ import shutil
 
 def copy_biggest_files(source, destination, number):
     """
-    This function copies n biggest files from source to destination folders.
+    This function copies n biggest files from source to destination folder.
     First draft.
     """
+    try:
+        os.chdir(source)
 
-    os.chdir(source)
+        if not (os.path.exists(source) and os.path.exists(destination)):
+            raise FileNotFoundError("You must specify proper source and destination folders")
 
-    files = tuple(
-        (os.path.abspath(file), os.stat(file).st_size, os.path.isfile(file)) for file in os.listdir(source)
-    )
-    sorted_files = sorted(files, key=lambda f: f[1], reverse=True)
+        if type(number) != int:
+            raise TypeError("Incorrect number of files specified (must be an integer)")
 
-    count = 0
-    number = number if number < len(sorted_files) else len(sorted_files)
-    while count < number:
-        file, size, is_file = sorted_files[count]
+    except FileNotFoundError as e:
+        print(e)
+    except TypeError as e:
+        print("{}\n{} is a {}".format(e, number, type(number).__name__))
+    else:
+        files = tuple(
+            (os.path.abspath(file), os.stat(file).st_size, os.path.isfile(file)) for file in os.listdir(source)
+        )
 
-        if is_file:
-            position = str(count).zfill(3)
-            print("[{}] Copying file: {}".format(position, file))
-            shutil.copy(file, destination)
+        sorted_files = sorted(files, key=lambda f: f[1], reverse=True)
 
-        count += 1
+        count = 0
+        number = number if number < len(sorted_files) else len(sorted_files)
+        while count < number:
+            file, size, is_file = sorted_files[count]
+
+            if is_file:
+                position = str(count).zfill(3)
+                print("[{}] Copying file: {}".format(position, file))
+                shutil.copy(file, destination)
+
+            count += 1
+        print("\t{} files copied from\n\t{} ==> {}".format(count, source, destination))
 
 
 source = "/home/neevor/"
 destination = os.path.join(os.getcwd(), "dest")
 
-copy_biggest_files(source, destination, 5)
-
+copy_biggest_files(source, destination, 4)
